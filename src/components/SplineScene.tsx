83132b178756c4a3e7b91e7d1e4e6679d8a56e57
P-arth-05 <163630266+P-arth-05@users.noreply.github.com>
2025-09-06 18:37:05 +0530

    Update SplineScene.tsx

--- src/components/SplineScene.tsx
+++ src/components/SplineScene.tsx
@@ -9,7 +9,7 @@ const SplineScene = () => {
       transition={{ duration: 2, ease: "easeOut" }}
       className="fixed inset-0 z-0"
     >
-      <Spline scene="https://prod.spline.design/uvLB18F1RJuOORs0/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/870dfT20yVREP7-L/scene.splinecode" />
     </motion.div>
   );
 };
